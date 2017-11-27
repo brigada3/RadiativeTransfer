85d5618b85d21d9ff7234ef60abc527c741fed03
Vadim Denisov <vadim.denisov.a@yandex.by>
2017-11-27 23:25:19 +0300

    Added logs in functions

--- b_system.py
+++ b_system.py
@@ -4,8 +4,8 @@
 import sympy
 
 
-N = 2001
-N1 = 240*N
+N = 20
+N1 = 24*N
 G = 0.999
 
 
@@ -17,12 +17,17 @@ def epsilon_x(s, r):
     return sympy.sqrt(s*(s+2*r))
 
 
-def kappa_x(s, r, w0):
+def kappa_x(s, r, w0, caller):
+    print('Enter kappa_x called by {}'.format(caller))
     math_func = (2*(s+r)+1) * (1-w0*f(s+r))
+    print('Kappa_x return ', math_func, end='\n\n')    
     return math_func
 
 
-def fi(v):
+def fi(v_arg):
+    print("Enter fi")
+    v = sympy.Symbol('v')
+
     math_func = sympy.Piecewise(
         (sympy.atan(sympy.im(v-1j)/sympy.re(v-1j)), sympy.re(v-1j) > 0),
         (-sympy.pi/2, sympy.And(sympy.Eq(sympy.re(v-1j), 0), sympy.im(v-1j) < 0)),
@@ -30,10 +35,12 @@ def fi(v):
         (-sympy.pi, sympy.And(sympy.re(v-1j)<0, sympy.Eq(sympy.im(v-1j), 0))),
         (-sympy.pi + sympy.atan(sympy.im(v-1j)/sympy.re(v-1j)), sympy.And(sympy.re(v-1j)<0, sympy.im(v-1j)>0))
     )
-    return math_func
+    print("fi result = ", math_func.subs(v, v_arg), end='\n\n')
+    return math_func.subs(v, v_arg)
 
 
-def fi_1(v):
+def fi_1(v_arg):
+    v = sympy.Symbol('v')    
     math_func = sympy.Piecewise(
         (sympy.atan(sympy.im(v+1j)/sympy.re(v+1j)), sympy.re(v+1j) > 0),
         (sympy.pi/2, sympy.And(sympy.Eq(sympy.re(v+1j), 0), sympy.im(v+1j) > 0)),
@@ -42,38 +49,56 @@ def fi_1(v):
         (sympy.pi + sympy.atan(sympy.im(v+1j)/sympy.re(v+1j)), sympy.And(sympy.re(v+1j)<0, sympy.im(v+1j)<0))
     )
 
-    return math_func
+    return math_func.subs(v, v_arg)
 
 
-def q_x(s, r, w0, n=N):
+def q_x(s_arg, r_arg, w0_arg, n_arg=N):
+    print("Enter q_x, called by gamma_x")    
+    print("Args: ", s_arg, r_arg, w0_arg)
+
+    s, r, n, w0 = sympy.symbols('s r n w0')
     math_func = sympy.Piecewise(
         ((s*(s+2*r))/((2*(s+r)+1)*(2*(s+r+1)+1)*(1-w0*f(s+r)*(1-w0*f(s+r+1)))), sympy.And(s+r+1<=n, n>=2)),
         ((s*(s+2*r))/((2*(s+r)+1)*(2*(s+r+1)+1)*(1-w0*f(s+r))), sympy.Eq(s+r, n)),
         ((s*(s+2*r))/((2*(s+r)+1)*(2*(s+r+1)+1)), s >= n-r+1)
     )
+    print("Result q_x: ", math_func.subs({s:s_arg, r:r_arg, n:n_arg, w0:w0_arg}), end='\n\n')
 
-    return math_func
+    return math_func.subs({s:s_arg, r:r_arg, n:n_arg, w0:w0_arg})
 
 
 def gamma(l, v, r, n=N, n1=N1):
+    print('Enter gamma')
     result = (
         (-1 +
             (sympy.exp(1)**(fi(v) + fi_1(v))*1j/2) *
             ( (1+sympy.re(v)**2-sympy.im(v)**2)**2 + 4*sympy.re(v)**2*sympy.im(v)**2)**0.5
         ) / 2 + 1
     )
+    print("init result = ", result)
 
-    for ind in range(n1, l-1, -1)
+    print("start ind iteration")
+    for ind in range(n1, l-1, -1):
         result = 1 + a(ind, v, r, n)/result
+        #print("ind: ", ind, " result: ", result)
 
+    print("final result: ", result, end='\n\n')
     return result
 
 
 def gamma_x(s, v, r, w0, n=N, n1=N1):
-    result = gamma(1, v, r, n, n1)]
+    print("Enter gamma_x")
+    result = gamma(1, v, r, n, n1)
+    print("Args gamma_x: s={} r={} v={}".format(s, r, v))    
+    print("init result via gamma = ", result)
 
+    print("start ind gamma_x iteration")
     for ind in range(n-r, s-1, -1):
-        result = 1 + q_x(s, r, w0, n)*v/result
+        print("ind: ", ind)
+        result = 1 + q_x(ind, r, w0, n)*v/result
+        print("new reuslt: ", result)
+    
+    print("Final result: ", result, end='\n\n')
     
     return result
 
@@ -89,11 +114,14 @@ def gamma_x(s, v, r, w0, n=N, n1=N1):
 #    return 1 + q_x(s, r, w0, n)*v/gamma_x_s_plus_1
 
 
-def h_x(alpha, v, r, w0, n=N, n1=N1):
+def h_x(alpha, v, r, w0, caller, n=N, n1=N1):
+    print("Enter h_x called by {}".format(caller))
+    print("Args h_x alpha={} v={} r={} w0={}".format(alpha, v, r, w0))
     math_func = (
         1j*v*epsilon_x(alpha, r) *
-        (kappa_x(alpha, r, w0) * gamma_x(alpha, v**2, r, w0, n, n1))**(-1)
+        (kappa_x(alpha, r, w0, "h_x") * gamma_x(alpha, v**2, r, w0, n, n1))**(-1)
     )
+    print("H_x return: ", math_func, end='\n\n')    
     return math_func
 
 
@@ -103,6 +131,8 @@ def a(l, v, r, n=N):
 
 
 def Px(i, r, mu):
+    print('Px')        
+    
     if i == r-1:
         return 0
     elif i == r:
@@ -120,15 +150,16 @@ def Px(i, r, mu):
     )
 
 
-def psi_x(j, v, r, w0, n=N, n1=N1):
-    l = sympy.symbols('l')
-
+def psi_x(j_arg, v, r, w0, n=N, n1=N1):
+    print('Enter psi_x called by b')
+    print('Args psi_x j={} v={} r={} w0={} n={} n1={}'.format(j_arg, v, r, w0, n, n1))
+    l, j = sympy.symbols('l j')
     math_func = sympy.Piecewise(
         ( 1, sympy.Eq(j, 0)),
-        ( sympy.Product(h_x(l, v, r, w0, n, n1), (l, 1, j)), True)
+        ( sympy.Product(h_x(l, v, r, w0, "psi_x", n, n1), (l, 1, j)).doit(), True)
     )
-
-    return math_func
+    print('psi_x return: ', math_func.subs(j, j_arg), end='\n\n')
+    return math_func.subs(j, j_arg)
 
 
 def nu_x(alpha, v, r, w0, mu_1, n=N, n1=N1):
@@ -137,7 +168,7 @@ def nu_x(alpha, v, r, w0, mu_1, n=N, n1=N1):
     elif alpha == n-r:
         return (
             ((2*n+1)*f(n)*Px(alpha+r, r, mu_1)) /
-            (kappa_x(n-r, r, w0)*gamma_x(n-r, v**2, r, w0, n, n1))
+            (kappa_x(n-r, r, w0, "nu_x")*gamma_x(n-r, v**2, r, w0, n, n1))
         )
     elif alpha.is_Number:    
         nu_x_alpha_plus_1 = nu_x(alpha+1, v, w0, mu_1, n, n1)
@@ -145,7 +176,7 @@ def nu_x(alpha, v, r, w0, mu_1, n=N, n1=N1):
         nu_x_alpha_plus_1 = sympy.Function('nu_x')(alpha+1, v, w0, mu_1, n, n1)
 
     return (
-        (kappa_x(alpha, r, w0)*gamma_x(alpha, v**2, r, w0, n, n1))**(-1)*
+        (kappa_x(alpha, r, w0, "nu_x")*gamma_x(alpha, v**2, r, w0, n, n1))**(-1)*
         (1j*v*epsilon_x(alpha+1, r)*nu_x_alpha_plus_1 + 
             (2*(alpha+r)+1)*f(alpha+r)*Px(alpha+r, r, mu_1)
         )
@@ -153,22 +184,25 @@ def nu_x(alpha, v, r, w0, mu_1, n=N, n1=N1):
 
 
 def b(alpha, v, w0, mu_1, r, n=N, n1=N1):
+    print('Enter b')
+    print('Args b: alpha={} v={} w0={} mu_1={} r={} n={} n1={}'.format(alpha, v, w0, mu_1, r, n, n1))    
     if alpha == 0 and n-r >= 0:
         j = sympy.symbols('j')
-        return (
-            (kappa_x(sympy.Integer('0'), r, w0)*gamma_x(sympy.Integer('0'), v**2, r, w0, n, n1))**(-1)*
+        result = (
+            (kappa_x(sympy.Integer('0'), r, w0, "b")*gamma_x(sympy.Integer('0'), v**2, r, w0, n, n1))**(-1)*
             sympy.Sum(
                 psi_x(j, v, r, w0, n, n1)*(2*(j+r)+1)*f(j+r)*Px(j+r, r, mu_1),
-                (j, 0, n-r)
-            )
+                (j, 0, n-r)).doit()
         )
+        print('b_0 =', result, end='\n\n')  
+        return result
     elif alpha.is_Number and 1 <= alpha <= n-r and n-r > 1:
         b_alpha_minus_1 = b(alpha-1, v, w0, mu_1, r, n, n1)
     else:
         b_alpha_minus_1 = sympy.Function('b')(alpha-1, v, w0, mu_1, n, n1)
     
     return (
-        h_x(alpha, v, r, w0, n, n1) * b_alpha_minus_1 +
+        h_x(alpha, v, r, w0, "b", n, n1) * b_alpha_minus_1 +
         nu_x(alpha, v, r, w0, mu_1, n, n1)
     )
 
--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import b_system
 
 def main():
     sympy.init_printing()
+    sys.setrecursionlimit(100000)
 
     s, r, w0, i, n = sympy.symbols('s r w0 i n')
     alpha, l, j, v = sympy.symbols('alpha l j v')
     mu, mu_1 = sympy.symbols('mu mu_1')
 
-    sympy.pprint(b_system.gumma(sympy.Integer('480240'), 1+2j, r))
-    sympy.pprint(b_system.gamma(sympy.Integer('480240'), 1+2j, r))
-    
+    for i in range(0, 21):
+        b_system.b(sympy.Integer(str(i)), -0.5j, 0.95, 0.8, sympy.Integer('0'))
 
 if __name__ == '__main__':
     main()
