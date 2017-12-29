a54b4a28ae19f6ff11c8243d8fc8ad0b1c7b63b0
Georgy Shapchits <georgy.schapchits@synesis.ru>
2017-12-29 23:43:03 +0300

    Small code refactoring

--- main.py
+++ main.py
@@ -4,40 +4,47 @@
 import sys
 import logging
 import scipy
-from b_system import b, F
+from b_system import F
+
+
+w0, mu, n, n1 = 0.95, 0.8, 1000, 600
+
+
+def calc_f_v_result(v):
+    F_v_result = 0
+
+    for r in range(0, n+1):
+        F_r_result = F(r=r, v=v, w0=w0, mu=mu, n=n, n1=n1)
+
+        fi, step = -scipy.pi, 0.1
+        while -scipy.pi <= fi <= scipy.pi:
+            F_v_result += F_r_result * scipy.exp(1j*r*fi)
+            fi += step
+
+        logging.info('F[r=%s][v=%s] = %s', r, v, F_r_result)
+
+    return F_v_result
 
 
 def main():
     sys.setrecursionlimit(10000000)
     logging.basicConfig(filename='log', filemode='w', level=logging.DEBUG)
 
-    w0, mu, n, n1 = 0.95, 0.8, 1000, 600
-
     F_v = {}
     v = -1 - 1j
     im_step, re_step = 0.1j, 0.5
-    for _ in range(101):
+    for i in range(101):
         v += im_step
         if not -1 < v.imag < 1:
             v = v - v.imag*1j + v.real + re_step
 
-        F_v_result = 0
-        for r in range(0, n+1):
-            F_r_result = F(r=r, v=v, w0=w0, mu=mu, n=n, n1=n1)
-
-            fi, step = -scipy.pi, 0.1
-            while -scipy.pi <= fi <= scipy.pi:
-                F_v_result += F_r_result * scipy.exp(1j*r*fi)
-                fi += step
-
-            logging.info('F[r=%s][v=%s] = %s', r, v, F_r_result)
+        F_v_result = calc_f_v_result(v)
 
         F_v[v] = F_v_result/(2*scipy.pi)
         logging.info('F[v=%s] = %s', v, F_v[v])
 
-    result = sum(F_v.values)
+    result = sum(F_v.values())
     logging.info('F_v sum: %s', result)
-
 
 
 if __name__ == '__main__':
